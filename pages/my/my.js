f5f6739f58cb85485fac8f1f3e9fe0d3bb2ff4c7
ALBAZ666 <ALBAZ666@users.noreply.github.com>
2026-06-30 12:52:44 +0800

    事件对象和传参

--- pages/my/my.js
+++ pages/my/my.js
@@ -1,16 +1,23 @@
-
 Page({
   bandleConsole(){
     console.log('我被点了')
   },
 
   handleParent(){
-    console.log('父元素被点击')
+    console.log("孩子被点了")
   },
 
   handleChildren(){
-    console.log('子元素被点击')
+    console.log('孩子被点击')
+  },
+
+  handleClick01(event){
+    // 如果有事件冒泡--》他们里面的值是不一样的
+    // currentTarget: 事件绑定者--->view
+    // target: 事件触发者--》view
+    console.log(event)
+    console.log(event.target.dataset)
+    console.log(event.target.dataset.name)
+    console.log(event.mark.name)
   }
 })
-
-
