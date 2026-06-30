7aedde42378ace05f4f48e4d266df208b70647ba
ALBAZ666 <ALBAZ666@users.noreply.github.com>
2026-06-30 18:31:16 +0800

    列表渲染

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,10 +3,13 @@ Page({
   data: {
     name: 'justin',
     age: 19,
-    userinfo: {
-      name: '小明',
-      hobby: '足球'
-    }
+    userinfo: { name: 'lqz', hobby: '烫头' },
+    names: ['神人', '人神', '神神', '人人'],
+    goodsList: [
+      { id: 1001, name: '钢笔', price: 9 },
+      { id: 1002, name: '铅笔', price: 6 },
+      { id: 1003, name: '脸盆', price: 99 }
+    ]
   },
   handleAddAge() {
     this.setData({
@@ -16,6 +19,13 @@ Page({
   handleChangeHobby() {
     this.setData({
       'userinfo.hobby': '篮球'
+    })
+  },
+  handleAddName() {
+    // 往 names 数组末尾追加一个新名字
+    const newNames = this.data.names.concat('新朋友')
+    this.setData({
+      names: newNames
     })
   }
 })
